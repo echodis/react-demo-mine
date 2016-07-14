452c395e72824b440d94bc9e0eb38f3cc77101b7
baidu <baidu@MacBook-Pro-2.local>
2016-07-14 16:44:32 +0800

    saved

---
+++ demo13/README.md
@@ -0,0 +1,13 @@
+This is my practice of [ruanyf's React.js Demo](https://github.com/ruanyf/react-demos)
+
+
+```bash
+# install the dependencies in demo13 directory
+$ npm install
+
+# translate all jsx file in src subdirectory to js file
+$ npm run build
+
+# launch http server
+$ node server.js
+```
---
+++ demo13/src/app.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default class App extends React.Component{
+	constructor(props) {
+		super(props);
+		this.render = this.render.bind(this);
+		this.state = {
+			items: this.props.items,
+			disabled: true
+		};
+	}
+
+	componentWillMount() {
+		this.setState({
+			disabled: false
+		}) 
+	}
+
+	handleClick() {
+		this.setState({
+			items: this.state.items.concat('Item' + this.state.items.length)
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.handleClick.bind(this)}>Add Item</button>
+				<ul>
+					{
+						this.state.items.map(function(item) {
+							return <li>{item}</li>
+						})
+					}
+				</ul>
+			</div>
+		)
+	}
+}
